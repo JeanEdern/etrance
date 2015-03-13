ac63d36cdf16617505b397fca58c74cdd3da3e4f
Jean-Edern Lorillon <trashjelo@gmail.com>
2015-03-13 20:04:29 +0100

    mailchimp

---
+++ lib/subscribe_to_newsletter.rb
@@ -0,0 +1,27 @@
+class SubscribeToNewsletter
+  def initialize
+    @gibbon = Gibbon::API.new(ENV['MAILCHIMP_API_KEY'])
+    @list_id = ENV['MAILCHIMP_LIST_ID']
+  end
+
+  def run(email)
+    @gibbon.lists.subscribe({
+      id: @list_id,
+      email: { email: email },
+      double_optin: false
+    })
+  end
+
+  # def run(params)
+  #   @gibbon.lists.subscribe({
+  #     id: @list_id,
+  #     email: { email: params[:email] },
+  #     merge_vars: {
+  #       FNAME: params[:first_name],
+  #       LNAME: params[:last_name]
+  #     },
+  #     double_optin: false,
+  #     update_existing: true
+  #   })
+  # end
+end
